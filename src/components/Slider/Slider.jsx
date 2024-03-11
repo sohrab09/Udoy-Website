3295153565535924f47a16a6fcc826a685b14be1
sohrab09 <sohrab.cse9@gmail.com>
2024-03-11 12:05:27 +0600

    Changs in design and slider image

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -9,6 +9,12 @@ import img1 from "../../assets/s1.jpg"
 import img2 from "../../assets/s2.jpg"
 import img3 from "../../assets/s3.jpg"
 import img4 from "../../assets/s4.jpg"
+import img5 from "../../assets/s5.jpg"
+import img6 from "../../assets/s6.jpg"
+import img7 from "../../assets/s7.jpg"
+import img8 from "../../assets/s8.jpg"
+import img9 from "../../assets/s9.jpg"
+import img10 from "../../assets/s10.jpg"
 
 const slide = [
     {
@@ -22,8 +28,25 @@ const slide = [
     },
     {
         img: img4
+    },
+    {
+        img: img5
+    },
+    {
+        img: img6
+    },
+    {
+        img: img7
+    },
+    {
+        img: img8
+    },
+    {
+        img: img9
+    },
+    {
+        img: img10
     }
-
 ]
 
 
@@ -38,13 +61,14 @@ const Slider = () => {
                 modules={[Autoplay, Pagination, Navigation]}
                 loop={true}
             >
-                {slide.map((item, index) => {
-                    return (
-                        <SwiperSlide key={index}>
-                            <img src={item.img} alt={item.img} className="swiper-slide" />
-                        </SwiperSlide>
-                    )
-                })
+                {
+                    slide.map((item, index) => {
+                        return (
+                            <SwiperSlide key={index}>
+                                <img src={item.img} alt={item.img} className="swiper-slide" />
+                            </SwiperSlide>
+                        )
+                    })
                 }
             </Swiper>
         </div>
